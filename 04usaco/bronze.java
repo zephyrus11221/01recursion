df3a3e2c4197151fef344feb7f6637f72835105c
Stephan Doan <sdoan@stuy.edu>
2016-02-25 00:20:22 -0500

    complete Bronze.java, fix KnightBoard.java

--- 04usaco/bronze.java
+++ 04usaco/bronze.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Bronze{
     private int[][] lake;
+    public int[] cmd;
     private int r, c, e, n;
     
+    public String name(){
+	return "Doan,Stephan";
+    }
+
     public Bronze (){
-	lake = new int[][] { { 28 25 20 32 34 36 } { 27 25 20 20 30 34 } { 24 20 20 20 20 30 } {20 20 14 14 20 20} };
+	lake = new int[][] { { 28, 25, 20, 32, 34, 36 }, { 27, 25, 20, 20, 30, 34 }, { 24, 20, 20, 20, 20, 30 }, {20, 20, 14, 14, 20, 20} };
 	r = 4;
 	c = 6;
 	e = 22;
@@ -11,7 +19,29 @@ public class Bronze{
     }
 
     public Bronze (String file){
-	Scanner in = new Scanner(file);
+	try{
+	Scanner in = new Scanner(new File(file));
+	r = in.nextInt();
+	c = in.nextInt();
+	e = in.nextInt();
+	n = in.nextInt();
+	System.out.println(e);
+	lake = new int[r][c];
+	for (int x = 0; x<r; x++){
+	    for (int y = 0; y<c; y++){
+		lake[x][y]=in.nextInt();
+	    }
+	}
+	cmd = new int[3*n];
+	for (int x = 0; x<n ; x++){
+	    cmd[x*3] = in.nextInt();
+	    cmd[x*3+1] = in.nextInt();
+	    cmd[x*3+2] = in.nextInt();
+	}
+	in.close();
+	}catch (FileNotFoundException e){
+	    System.out.println("File not found. Try a different one.");
+	}
     }
 
     public void dig (int x, int y, int d){
@@ -31,6 +61,26 @@ public class Bronze{
 	}
     }
     
+    public String solve(){
+	int sum = 0;
+	String otpt = "";
+	for (int x = 0; x<n; x++){
+	    dig(cmd[x*3]-1, cmd[x*3+1]-1, cmd[x*3+2]);
+	    System.out.println(this);
+	}
+	for (int x = 0; x<r; x++){
+	    for (int y = 0; y<c; y++){
+		if (e>lake[x][y]){
+		    sum+=e-lake[x][y];
+		}
+	    }
+	}
+	otpt+= sum*5184;
+	otpt+= ",7,"+name();
+	return otpt;
+
+    }
+    
     public String toString(){
 	String otpt = "";
 	for (int x = 0; x < r; x++){
@@ -48,5 +98,9 @@ public class Bronze{
 	    otpt += "\n";
 	}
 	return otpt;
+    }
+    public static void main(String[]args){
+	Bronze a = new Bronze("makelake.in");
+	System.out.println(a.solve());
     }
 }
