aeecfcce4aec759e28014138b245862340bed403
Stephan Doan <sdoan@stuy.edu>
2016-04-06 00:06:29 -0400

    add expand(), compiles, untested

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -15,11 +15,29 @@ public class MyDeque<T>{
 	return data[start];
     }
     
+    @SuppressWarnings("unchecked")	    
     public void expand(){
 	int ctr = 0;
 	if (start<end){
-	    ctr = end-start;
+	    ctr = end-start+1;
+	}else if(start==end){
+	    ctr = 1;
+	}else{
+	    ctr = data.length-start+end;
 	}
+	T[] copy = (T[]) new Object[10];
+	int x = 0;
+	while (x<ctr){
+	    copy[x]=data[start];
+	    x++;
+	    if(start==data.length-1){
+		start = 0;
+	    }else{
+		start++;
+	    }
+	}
+	end = x;
+	start = 0;
     }
     public static void main(String[]args){
 	MyDeque<String> x = new MyDeque<String>();
